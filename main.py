d30013ec900d9541542675306f4f6b5cdc959cb3
AhmedRayyan44 <165449090+AhmedRayyan44@users.noreply.github.com>
2024-08-10 15:59:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ product_send_times = {}
 special_products = ["بيربل مست", "هايلاند بيريز", "سبايسي زيست"]
 
 # List of products to exclude from sending
-# excluded_products = ["هيلة", "تمرة", "سمرة"]
-excluded_products = [""]
+excluded_products = ["ايسي رش", "سي سايد فروست", "سمرة"]
 
 # Variable to store the time of the last clearing of the sent_products list
 last_clear_time = time.time()
@@ -43,7 +42,7 @@ def extract_product_details(product_url):
         html_content = response.content
         soup = BeautifulSoup(html_content, "html.parser")
         product_name = soup.find("span", class_="base", itemprop="name").text.strip()
-        product_status_element = soup.find("div", class_="stock available").span
+        product_status_element = soup.find("div", class_="stock unavailable").span
         product_status = product_status_element.text.strip() if product_status_element else None
 
         # Extract all image URLs and find the one containing the desired pattern
@@ -55,7 +54,6 @@ def extract_product_details(product_url):
             if src and pattern in src:
                 image_url = src
                 break
-
         return product_name, product_status, image_url
     except Exception as e:
         print(f"An error occurred while extracting product details for {product_url}: {str(e)}")
@@ -70,7 +68,6 @@ def send_product_data_to_telegram():
         soup = BeautifulSoup(html_content, "html.parser")
         product_links = [a["href"] for a in soup.find_all("a", class_="product-item-link")]
         product_data_list = []
-
         for product_link in product_links:
             product_info = {"url": product_link}
             product_name, product_status, image_url = extract_product_details(product_link)
@@ -81,25 +78,25 @@ def send_product_data_to_telegram():
                 print(f"Product Status: {product_status}")
                 print(f"Image URL: {image_url}")
                 print("-" * 50)
-
+        
         bot_token = "7255964467:AAG0G3rU0QxMq--_6rv7kxcGvHI-r0hDDTw"
         chat_id = "@Dezerrt"
         telegram_api_url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
-
+        
         for product_data in product_data_list:
             product_name = product_data.get("name", "")
             product_status = product_data.get("status", "")
             product_url = product_data.get("url", "")
             image_url = product_data.get("image_url", "")
-
-            if product_status == "متوفر" and product_name not in excluded_products:
+            
+            if product_status == "سيتم توفيرها في المخزون قريباً" and product_name not in excluded_products:
                 current_time = time.time()
                 if product_name in special_products:
                     if (product_name not in sent_products) or (current_time - product_send_times.get(product_name, 0) >= (3 * 600)):
-                        message_text = f"✅ **المنتج متاح** ✅: {product_name}"
+                        message_text = f"✅ ** المنتج متاح ** ✅: {product_name}"
                         reply_markup = {
                             "inline_keyboard": [
-                                [{"text": "🔍 عرض المنتج", "url": product_link}, {"text": "🛒 عرض السلة", "url": "https://www.dzrt.com/ar/checkout/cart"}],
+                                [{"text": "🔍 عرض المنتج", "url": product_url}, {"text": "🛒 عرض السلة", "url": "https://www.dzrt.com/ar/checkout/cart"}],
                                 [{"text": "🔐 تسجيل الدخول", "url": "https://www.dzrt.com/ar/customer/account/login/"}, {"text": "💳 الانتقال إلى رابط الدفع النهائي", "url": "https://www.dzrt.com/ar/onestepcheckout.html"}]
                             ]
                         }
@@ -118,10 +115,10 @@ def send_product_data_to_telegram():
                             print(f"Failed to send product data for {product_name}. Status code: {response.status_code}")
                 else:
                     if product_name not in sent_products:
-                        message_text = f"✅ **المنتج متاح** ✅: {product_name}"
+                        message_text = f"✅ ** المنتج متاح ** ✅: {product_name}"
                         reply_markup = {
                             "inline_keyboard": [
-                                [{"text": "🔍 عرض المنتج", "url": product_link}, {"text": "🛒 عرض السلة", "url": "https://www.dzrt.com/ar/checkout/cart"}],
+                                [{"text": "🔍 عرض المنتج", "url": product_url}, {"text": "🛒 عرض السلة", "url": "https://www.dzrt.com/ar/checkout/cart"}],
                                 [{"text": "🔐 تسجيل الدخول", "url": "https://www.dzrt.com/ar/customer/account/login/"}, {"text": "💳 الانتقال إلى رابط الدفع النهائي", "url": "https://www.dzrt.com/ar/onestepcheckout.html"}]
                             ]
                         }
@@ -137,7 +134,7 @@ def send_product_data_to_telegram():
                             sent_products.append(product_name)
                         else:
                             print(f"Failed to send product data for {product_name}. Status code: {response.status_code}")
-
+        
         if time.time() - last_clear_time >= 60:
             sent_products = [product for product in sent_products if product in special_products]
             last_clear_time = time.time()
@@ -145,4 +142,4 @@ def send_product_data_to_telegram():
 # Main loop to run the code every minute
 while True:
     send_product_data_to_telegram()
-    time.sleep(1)
+    time.sleep(10)
